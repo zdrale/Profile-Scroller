ee50d454f9b020ebf34b39eb611ad017e8106532
zdrale <nikolazd22@outlook.com>
2020-12-17 13:37:20 +0100

    Added some hardcoded data for users

---
+++ app.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    name:'John Doe',
+    age:32,
+    gender:'male',
+    lookingFor:'female',
+    location:'Boston MA',
+    image:'https://randomuser.me/api/portraits/men/82.jpg'
+  },
+  {
+    name:'Jehn Smith',
+    age:26,
+    gender:'female',
+    lookingFor:'male',
+    location:'Miami FL',
+    image:'https://randomuser.me/api/portraits/women/42.jpg'
+  },
+  {
+    name:'Will Joe',
+    age:38,
+    gender:'male',
+    lookingFor:'female',
+    location:'Lynn MA',
+    image:'https://randomuser.me/api/portraits/men/87.jpg'
+  }
+];
